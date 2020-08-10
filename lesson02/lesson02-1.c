987a54ade8a12e2300a1e779e49ed8d6f1b1ad30
but-sergey <but-sergey@bk.ru>
2020-08-10 15:42:12 +0300

    Решето Эратосфена

--- lesson02/lesson02-1.c
+++ lesson02/lesson02-1.c
@@ -51,12 +51,30 @@ int main(int argc, char** args)
     int b;
     int coeffA;
     int coeffB;
-    printf("Input number A: ");
-    scanf("%d", &a);
-    printf("Input number B: ");
-    scanf("%d", &b);
-    printf("NOD = %d\n", euclidusExt(a, b, &coeffA, &coeffB));
-    printf("ax + by = %d\n", a * coeffA + b * coeffB);
+    // printf("Input number A: ");
+    // scanf("%d", &a);
+    // printf("Input number B: ");
+    // scanf("%d", &b);
+    // printf("NOD = %d\n", euclidusExt(a, b, &coeffA, &coeffB));
+    // printf("ax + by = %d\n", a * coeffA + b * coeffB);
+    int arr[255] = {2, 3};
+    int capacity = 2;
+    for(int i = 4; i < 255; ++i) {
+        int isPrime = 1;
+        for(int j = 0; j < capacity; ++j) {
+            if(i % arr[j] == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+        if(isPrime) {
+            arr[capacity++] = i;
+        }
+    }
+
+    for(int k = 0; k < capacity; ++k) {
+        printf("%d ", arr[k]);
+    }
  
     return 0;
 }
