cdc71c6097407092ab838a441b4a023d5648a886
but-sergey <but-sergey@bk.ru>
2020-08-14 16:23:20 +0300

    В библиотеку добавлены функции поиска в массиве

--- lesson04/geek.c
+++ lesson04/geek.c
@@ -58,3 +58,131 @@ void swapInt(int *a, int *b) {
     *a = *b;
     *b = t;
 }
+
+void bubbleSort(int* arr, int len) {
+    for(int i = 0; i < len; ++i) {
+        for(int j = 0; j < len - 1; ++j) {
+            if(arr[j] > arr[j + 1]) {
+                swapInt(&arr[j], &arr[j + 1]);
+            }
+        }
+    }
+}
+
+void shakerSort(int* arr, int len) {
+    int left = 1;
+    int right = len - 1;
+    while(left <= right) {
+        int i;
+        for(i = right; i >= left; --i) {
+            if(arr[i - 1] > arr[i]) {
+                swapInt(&arr[i], &arr[i - 1]);
+            }
+        }
+        left++;
+        for(i = left; i <= right; ++i) {
+            if(arr[i - 1] > arr[i]) {
+                swapInt(&arr[i], &arr[i - 1]);
+            }
+        }
+        right--;
+    }
+}
+
+void sortInserts(int* arr, int len) {
+    int temp, pos;
+    for(int i = 1; i < len; ++i) {
+        temp = arr[i];
+        pos = i - 1;
+        while(pos >= 0 && arr[pos] > temp) {
+            arr[pos + 1] = arr[pos];
+            pos--;
+        }
+        arr[pos + 1] = temp;
+    }
+}
+
+void sortSelection(int* arr, int len) {
+    int min;
+    for(int i = 0; i < len - 1; ++i) {
+        min = i;
+        for(int j = i + 1; j < len; ++j) {
+            if(arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        swapInt(&arr[i], &arr[min]);
+    }
+}
+
+int linearSearch(int* arr, int len, int value) {
+    int i = 0;
+    while(i < len && arr[i] != value) {
+        i++;
+    }
+
+    if(i < len) {
+        return i;
+    } else {
+        return -1;
+    }
+}
+
+int bareerSearch(int* arr, int len, int value) {
+    int i = 0;
+    while(arr[i] != value) {
+        i++;
+    }
+
+    if(i < len - 1) {
+        return i;
+    } else {
+        return -1;
+    }
+}
+
+int binarySearch(int* arr, int len, int value) {
+    int l = 0;
+    int r = len - 1;
+    int mid = (l + r) / 2;
+
+    while(l <= r && arr[mid] != value) {
+        if(arr[mid] < value) {
+            l = mid;
+        } else {
+            r = mid;
+        }
+        mid = (l + r) / 2;
+    }
+
+    if(arr[mid] == value){
+        return mid;
+    } else {
+        return -1;
+    }
+}
+
+int searchInter(int* arr, int len, int value) {
+    int mid;
+    int l = 0;
+    int r = len - 1;
+
+    while(arr[l] < value && arr[r] > value) {
+        mid = l + ((value - arr[l]) * (r - l)) / (arr[r] - arr[l]);
+        if(arr[mid] < value) {
+            l = mid + 1;
+        } else if(arr[mid] > value) {
+            r = mid - 1;
+        } else {
+            return mid;
+        }
+    }
+
+    if(arr[l] == value) {
+        return l;
+    } else if (arr[r] = value) {
+        return r;
+    } else {
+        return -1;
+    }
+}
--- lesson04/geek.h
+++ lesson04/geek.h
@@ -6,19 +6,26 @@
 #include "geek.c"
 
 void printIntArray(int* array, int size, int offset);
+void print2dIntArray(int** array, const int row, const int col, int offset);
 
 int** init2dIntArray(int** array, const int row, const int col);
 
-void print2dIntArray(int** array, const int row, const int col, int offset);
-
 void fillIntRandom(int* arr, int len, int border);
-
 void fill2dIntArray(int** array, const int row, const int col);
 
 int get2dInt(int** array, const int row, const int col);
-
 void set2dInt(int** array, const int row, const int col, int value);
 
 void swapInt(int *a, int *b);
+
+void bubbleSort(int* arr, int len);
+void shakerSort(int* arr, int len);
+void sortInserts(int* arr, int len);
+void sortSelection(int* arr, int len);
+
+int linearSearch(int* arr, int len, int value);
+int bareerSearch(int* arr, int len, int value);
+int binarySearch(int* arr, int len, int value);
+int searchInter(int* arr, int len, int value);
 
 #endif
